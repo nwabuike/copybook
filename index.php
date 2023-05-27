20dd1f2d6e62335bc6d87aeed076401de9c50ee8
nwabuike <nwabuikeanthony@gmail.com>
2023-05-27 21:59:53 +0100

    fr

--- index.php
+++ index.php
@@ -31,7 +31,8 @@
     <link rel="stylesheet" href="maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 
     <!-- GOOGLE FONTS -->
-    <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="fonts/font.css">
+    <!-- <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'> -->
 
     <!-- PLUGINS STYLESHEET -->
     <link rel="stylesheet" href="css/plugins/magnific-popup.css">
