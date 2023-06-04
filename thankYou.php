40f953cfc8c7d9c6fec9af628ff795d6894486de
nwabuike <nwabuikeanthony@gmail.com>
2023-06-04 14:40:16 +0100

    THANK

--- thankYou.php
+++ thankYou.php
@@ -59,12 +59,7 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <script>
-        fbq('track', 'Purchase', {
-            value: 5000,
-            currency: '5000',
-        });
-    </script>
+
 
 </head>
 
@@ -185,6 +180,12 @@
 
     <!-- Back to Top Button -->
     <a href="#" class="top" style="background-color:#8e44ad;">Top</a>
+    <script>
+        fbq('track', 'Purchase', {
+            value: 5000,
+            currency: 'NGN',
+        });
+    </script>
 
 
     <!-- =========================
